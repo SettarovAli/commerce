bbbbf0e7830f4d198f1c8eee2ac4d7212174d841
SettarovAli <settarov.ali@gmail.com>
2024-07-08 01:23:25 +0300

    feat: update input component with error message

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -6,9 +6,10 @@ type InputProps = {
   value: string;
   onChange: React.Dispatch<React.SetStateAction<string>>;
   type?: HTMLInputTypeAttribute;
+  error?: string;
 };
 
-const Input: React.FC<InputProps> = ({ name, label, value, onChange, type }) => {
+const Input: React.FC<InputProps> = ({ name, label, value, onChange, type, error }) => {
   return (
     <div>
       <label htmlFor={name} className="block text-sm font-medium leading-6 text-gray-900">
@@ -22,6 +23,7 @@ const Input: React.FC<InputProps> = ({ name, label, value, onChange, type }) => 
         onChange={(e) => onChange(e.target.value)}
         className="mt-2 block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
       />
+      {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
     </div>
   );
 };
