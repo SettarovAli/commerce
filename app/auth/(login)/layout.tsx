b4ce054709a05f2c477bdc5bd89f41a94a3f8095
SettarovAli <settarov.ali@gmail.com>
2024-08-06 14:49:58 +0300

    fix: delete layout for auth redirects

--- app/auth/(login)/layout.tsx
+++
@@ -1,8 +0,0 @@
-import AuthUser from '@/modules/auth/auth-user';
-import { PropsWithChildren } from 'react';
-
-const AuthLoginLayout: React.FC<PropsWithChildren> = ({ children }) => {
-  return <AuthUser redirectLoggedInUser>{children}</AuthUser>;
-};
-
-export default AuthLoginLayout;
