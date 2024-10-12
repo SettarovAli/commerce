7800b3d0a6a77e026b17e47f12ba00e5f319b888
SettarovAli <settarov.ali@gmail.com>
2024-10-13 01:18:09 +0300

    feat: update change email form with server action

--- modules/account/change-email-form.tsx
+++ modules/account/change-email-form.tsx
@@ -1,70 +1,31 @@
-import Button from '@/components/button';
-import Input from '@/components/input';
-import { updateUserEmail } from '@/lib/firebase/auth/email';
-import { ValidationError, ValidationFields, handleValidation } from '@/lib/zod';
-import { User } from 'firebase/auth';
+import Input from 'components/input';
+import SubmitButton from 'components/submit-button';
+import { useAuthAction } from 'hooks/use-auth-action';
+import { updateEmailAction } from 'lib/firebase/auth/server-actions';
+import { authService } from 'lib/firebase/auth/service';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { z } from 'zod';
+import { Routes } from 'routes';
 
-const Schema = z.object({
-  email: ValidationFields.EMAIL,
-  newEmail: ValidationFields.EMAIL,
-  password: ValidationFields.PASSWORD
-});
+type AuthActionData = Record<'newEmail' | 'password', string>;
 
-type ChangeEmailFormProps = {
-  user: User;
-};
-
-const ChangeEmailForm: React.FC<ChangeEmailFormProps> = ({ user }) => {
-  const [email, setEmail] = useState(user?.email as string);
-  const [newEmail, setNewEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [errors, setErrors] = useState<ValidationError<typeof Schema>>({});
-
+const ChangeEmailForm: React.FC = () => {
   const router = useRouter();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    handleValidation({
-      data: { email, newEmail, password },
-      schema: Schema,
-      onError: setErrors,
-      onSuccess: (res) => {
-        updateUserEmail(res.email, res.newEmail, res.password, setIsLoading, router);
-        setErrors({});
-      }
-    });
-  };
+  const { action, errors } = useAuthAction({
+    authAction: updateEmailAction,
+    clientAction: async (data: AuthActionData) => {
+      return await authService.updateEmail(data.newEmail, data.password);
+    },
+    onSuccess: () => {
+      router.push(Routes.SignIn);
+    }
+  });
 
   return (
-    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-      <Input
-        label="Current Email address"
-        name="email"
-        value={email}
-        onChange={setEmail}
-        error={errors.email}
-      />
-      <Input
-        label="New Email address"
-        name="new-email"
-        value={newEmail}
-        onChange={setNewEmail}
-        error={errors.newEmail}
-      />
-      <Input
-        label="Password"
-        name="password"
-        value={password}
-        onChange={setPassword}
-        error={errors.password}
-      />
-      <Button type="submit" isLoading={isLoading}>
-        Change Email
-      </Button>
+    <form className="flex flex-col gap-4" action={action} noValidate>
+      <Input type="email" name="new-email" label="New email address" errors={errors.newEmail} />
+      <Input type="password" name="password" label="Password" errors={errors.password} />
+      <SubmitButton>Change Email</SubmitButton>
     </form>
   );
 };
