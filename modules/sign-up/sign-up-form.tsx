bef0343d28168698197a710e82ceda4ba1089819
SettarovAli <settarov.ali@gmail.com>
2024-10-13 01:09:58 +0300

    feat: update sign up form with server action

--- modules/sign-up/sign-up-form.tsx
+++ modules/sign-up/sign-up-form.tsx
@@ -1,67 +1,28 @@
 'use client';
 
-import Button from '@/components/button';
-import Input from '@/components/input';
-import { signUpUser } from '@/lib/firebase/auth/email';
-import { ValidationError, ValidationFields, handleValidation } from '@/lib/zod';
+import Input from 'components/input';
+import SubmitButton from 'components/submit-button';
+import { useAuthAction } from 'hooks/use-auth-action';
+import { signUpAction } from 'lib/firebase/auth/server-actions';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { z } from 'zod';
-
-const Schema = z.object({
-  email: ValidationFields.EMAIL,
-  password: ValidationFields.PASSWORD,
-  name: ValidationFields.REQUIRED
-});
+import { Routes } from 'routes';
 
 const SignUpForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [errors, setErrors] = useState<ValidationError<typeof Schema>>({});
-
   const router = useRouter();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    handleValidation({
-      data: { name, email, password },
-      schema: Schema,
-      onError: setErrors,
-      onSuccess: (res) => {
-        signUpUser(res.name, res.email, res.password, setIsLoading, router);
-        setErrors({});
-      }
-    });
-  };
+  const { action, errors } = useAuthAction({
+    authAction: signUpAction,
+    onSuccess: () => {
+      router.push(Routes.SignIn);
+    }
+  });
 
   return (
-    <form className="mt-10 flex flex-col gap-4" onSubmit={handleSubmit} noValidate>
-      <Input
-        label="First Name"
-        name="username"
-        value={name}
-        onChange={setName}
-        error={errors.name}
-      />
-      <Input
-        label="Email address"
-        name="email"
-        value={email}
-        onChange={setEmail}
-        error={errors.email}
-      />
-      <Input
-        label="Password"
-        name="password"
-        value={password}
-        onChange={setPassword}
-        error={errors.password}
-      />
-      <Button type="submit" isLoading={isLoading}>
-        Sign up
-      </Button>
+    <form className="mt-10 flex flex-col gap-4" action={action} noValidate>
+      <Input type="text" name="name" label="First Name" errors={errors.name} />
+      <Input type="email" name="email" label="Email address" errors={errors.email} />
+      <Input type="password" name="password" label="Password" errors={errors.password} />
+      <SubmitButton>Sign up</SubmitButton>
     </form>
   );
 };
