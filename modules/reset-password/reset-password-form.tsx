fdb8053f8fd2ccff53c7fa2b2d414059d40d3510
SettarovAli <settarov.ali@gmail.com>
2024-10-13 01:15:33 +0300

    feat: update reset password form with server action

--- modules/reset-password/reset-password-form.tsx
+++ modules/reset-password/reset-password-form.tsx
@@ -1,16 +1,11 @@
 'use client';
 
-import Button from '@/components/button';
 import Input from '@/components/input';
-import { resetPassword } from '@/lib/firebase/auth/password';
-import { ValidationError, ValidationFields, handleValidation } from '@/lib/zod';
+import SubmitButton from '@/components/submit-button';
+import { useAuthAction } from '@/hooks/use-auth-action';
+import { resetPasswordAction } from '@/lib/firebase/auth/server-actions';
+import { Routes } from '@/routes';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { z } from 'zod';
-
-const Schema = z.object({
-  newPassword: ValidationFields.PASSWORD
-});
 
 type Props = {
   actionCode: string;
@@ -19,38 +14,19 @@ type Props = {
 const ResetPasswordForm: React.FC<Props> = (props) => {
   const { actionCode } = props;
 
-  const [newPassword, setNewPassword] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [errors, setErrors] = useState<ValidationError<typeof Schema>>({});
-
   const router = useRouter();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    handleValidation({
-      data: { newPassword },
-      schema: Schema,
-      onError: setErrors,
-      onSuccess: (res) => {
-        resetPassword(actionCode, res.newPassword, setIsLoading, router);
-        setErrors({});
-      }
-    });
-  };
+  const { action, errors } = useAuthAction({
+    authAction: resetPasswordAction.bind(null, actionCode),
+    onSuccess: () => {
+      router.push(Routes.SignIn);
+    }
+  });
 
   return (
-    <form className="mx-auto max-w-3xl space-y-6" onSubmit={handleSubmit} noValidate>
-      <Input
-        label="New password"
-        name="new-password"
-        type="password"
-        value={newPassword}
-        onChange={setNewPassword}
-        error={errors.newPassword}
-      />
-      <Button type="submit" isLoading={isLoading}>
-        Reset Password
-      </Button>
+    <form className="mx-auto max-w-3xl space-y-6" action={action} noValidate>
+      <Input type="password" name="new-password" label="New password" errors={errors.newPassword} />
+      <SubmitButton>Reset Password</SubmitButton>
     </form>
   );
 };
