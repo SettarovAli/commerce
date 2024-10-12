faf05030183ad93012a0c58f7203ee8a76629740
SettarovAli <settarov.ali@gmail.com>
2024-10-13 01:12:25 +0300

    feat: update sign in form with server action

--- modules/sign-in/sign-in-form.tsx
+++ modules/sign-in/sign-in-form.tsx
@@ -1,66 +1,40 @@
 'use client';
 
-import Button from '@/components/button';
-import Input from '@/components/input';
-import { loginUserWithEmailAndPassword } from '@/lib/firebase/auth/email';
-import { ValidationError, ValidationFields, handleValidation } from '@/lib/zod';
-import { Routes } from '@/routes';
+import Input from 'components/input';
+import SubmitButton from 'components/submit-button';
+import { useAuthAction } from 'hooks/use-auth-action';
+import { signInAction } from 'lib/firebase/auth/server-actions';
+import { authService } from 'lib/firebase/auth/service';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { z } from 'zod';
+import { Routes } from 'routes';
 
-const Schema = z.object({
-  email: ValidationFields.EMAIL,
-  password: ValidationFields.PASSWORD
-});
+type AuthActionData = Record<'email' | 'password', string>;
 
 const SignInForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [errors, setErrors] = useState<ValidationError<typeof Schema>>({});
-
   const router = useRouter();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    handleValidation({
-      data: { email, password },
-      schema: Schema,
-      onError: setErrors,
-      onSuccess: (res) => {
-        loginUserWithEmailAndPassword(res.email, res.password, setIsLoading, router);
-        setErrors({});
-      }
-    });
-  };
+  const { action, errors } = useAuthAction({
+    authAction: signInAction,
+    clientAction: async (data: AuthActionData) => {
+      return await authService.signInWithEmailAndPassword(data.email, data.password);
+    },
+    onSuccess: () => {
+      router.push(Routes.Home);
+    }
+  });
 
   return (
-    <form className="mt-10 flex flex-col gap-4" onSubmit={handleSubmit} noValidate>
-      <Input
-        label="Email address"
-        name="email"
-        value={email}
-        onChange={setEmail}
-        error={errors.email}
-      />
-      <Input
-        label="Password"
-        name="password"
-        value={password}
-        onChange={setPassword}
-        error={errors.password}
-      />
+    <form className="mt-10 flex flex-col gap-4" action={action} noValidate>
+      <Input type="email" name="email" label="Email address" errors={errors.email} />
+      <Input type="password" name="password" label="Password" errors={errors.password} />
       <Link
         href={Routes.ForgotPassword}
         className="flex justify-end self-end text-sm font-semibold leading-6 text-blue-600 hover:text-blue-500"
       >
         Forgot Password
       </Link>
-      <Button type="submit" isLoading={isLoading}>
-        Sign In
-      </Button>
+      <SubmitButton>Sign In</SubmitButton>
     </form>
   );
 };
