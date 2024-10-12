fbeedc4a4c48b942c6e2efbe08a3d6087e2d372e
SettarovAli <settarov.ali@gmail.com>
2024-10-12 23:27:37 +0300

    feat: update input component for server actions

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,30 +1,33 @@
 import { HTMLInputTypeAttribute } from 'react';
 
 type InputProps = {
-  label: string;
+  type: HTMLInputTypeAttribute;
   name: string;
-  value: string;
-  onChange: React.Dispatch<React.SetStateAction<string>>;
-  type?: HTMLInputTypeAttribute;
-  error?: string;
+  label?: string;
+  value?: string;
+  onChange?: React.Dispatch<React.SetStateAction<string>>;
+  errors?: string[];
 };
 
-const Input: React.FC<InputProps> = ({ name, label, value, onChange, type, error }) => {
+const Input: React.FC<InputProps> = ({ name, label, value, onChange, type, errors }) => {
   return (
     <div>
-      <label htmlFor={name} className="block text-sm font-medium leading-6 text-gray-900">
-        {label}
-      </label>
+      {label && (
+        <label htmlFor={name} className="block text-sm font-medium leading-6 text-gray-900">
+          {label}
+        </label>
+      )}
       <input
-        type={type || name}
+        type={type}
+        name={name}
         id={name}
         required
-        value={value}
         autoComplete={name}
-        onChange={(e) => onChange(e.target.value)}
         className="mt-2 block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
+        value={value}
+        {...(onChange ? { onChange: (e) => onChange(e.target.value) } : {})}
       />
-      {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
+      {errors && <p className="mt-1 text-sm text-red-500">{errors.join('. ')}</p>}
     </div>
   );
 };
