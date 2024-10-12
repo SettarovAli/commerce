3fc56c07ecf7d81c37349f609193466674ff9ce7
SettarovAli <settarov.ali@gmail.com>
2024-10-13 01:33:45 +0300

    feat: update delete google user with server action

--- modules/account/delete-google-user.tsx
+++ modules/account/delete-google-user.tsx
@@ -1,20 +1,26 @@
-import Button from '@/components/button';
-import { deleteUserFromFirestore } from '@/lib/firebase/auth/delete-user';
+import Button from 'components/button';
+import { authService } from 'lib/firebase/auth/service';
+import { handleAuthActionResult } from 'lib/firebase/auth/utils/handle-auth-action-result';
 import { useRouter } from 'next/navigation';
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
+import { Routes } from 'routes';
 
 const DeleteGoogleUser: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
   const router = useRouter();
 
-  const handleDeleteGoogleUser = useCallback(() => {
-    deleteUserFromFirestore(router, setIsLoading);
+  const handleDeleteGoogleUser = useCallback(async () => {
+    const authActionResult = await authService.deleteGoogleUser();
+    handleAuthActionResult({
+      authActionResult,
+      onSuccess: () => {
+        router.push(Routes.SignIn);
+      }
+    });
   }, [router]);
 
   return (
     <div className="mt-4 flex flex-col gap-4 text-sm text-gray-500">
-      <Button type="button" onClick={handleDeleteGoogleUser} isLoading={isLoading} variant="red">
+      <Button type="button" onClick={handleDeleteGoogleUser} variant="red">
         Confirm delete
       </Button>
     </div>
