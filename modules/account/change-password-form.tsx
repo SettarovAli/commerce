ff64143cced0d391f65598426e7dcdb4c838d5ef
SettarovAli <settarov.ali@gmail.com>
2024-10-13 01:31:19 +0300

    feat: update change password form with server action

--- modules/account/change-password-form.tsx
+++ modules/account/change-password-form.tsx
@@ -1,66 +1,34 @@
-import Button from '@/components/button';
-import Input from '@/components/input';
-import { updateUserPassword } from '@/lib/firebase/auth/password';
-import { ValidationError, ValidationFields, handleValidation } from '@/lib/zod';
-import { useState } from 'react';
-import { z } from 'zod';
-
-const Schema = z.object({
-  currentPassword: ValidationFields.PASSWORD,
-  newPassword: ValidationFields.PASSWORD
-});
+import Input from 'components/input';
+import SubmitButton from 'components/submit-button';
+import { useAuthAction } from 'hooks/use-auth-action';
+import { updatePasswordAction } from 'lib/firebase/auth/server-actions';
+import { authService } from 'lib/firebase/auth/service';
 
 type Props = {
   onCloseModal: () => void;
 };
 
+type AuthActionData = Record<'password' | 'newPassword', string>;
+
 const ChangePasswordForm: React.FC<Props> = (props) => {
   const { onCloseModal } = props;
 
-  const [currentPassword, setCurrentPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [errors, setErrors] = useState<ValidationError<typeof Schema>>({});
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    handleValidation({
-      data: { currentPassword, newPassword },
-      schema: Schema,
-      onError: setErrors,
-      onSuccess: (res) => {
-        updateUserPassword(res.currentPassword, res.newPassword, setIsLoading, onCloseModal);
-        setErrors({});
-      }
-    });
-  };
+  const { action, errors } = useAuthAction({
+    authAction: updatePasswordAction,
+    clientAction: async (data: AuthActionData) => {
+      return await authService.updatePassword(data.password, data.newPassword);
+    },
+    onSuccess: () => {
+      onCloseModal();
+    }
+  });
 
   return (
-    <form
-      className="mt-4 flex flex-col gap-4 text-sm text-gray-500"
-      onSubmit={handleSubmit}
-      noValidate
-    >
+    <form className="mt-4 flex flex-col gap-4 text-sm text-gray-500" action={action} noValidate>
       Please enter your password to delete your account:
-      <Input
-        label="Current Password"
-        name="password"
-        value={currentPassword}
-        onChange={setCurrentPassword}
-        type="password"
-        error={errors.currentPassword}
-      />
-      <Input
-        label="New Password"
-        name="new-password"
-        value={newPassword}
-        onChange={setNewPassword}
-        type="password"
-        error={errors.newPassword}
-      />
-      <Button type="submit" isLoading={isLoading}>
-        Confirm password change
-      </Button>
+      <Input type="password" name="password" label="Current Password" errors={errors.password} />
+      <Input type="password" name="new-password" label="New Password" errors={errors.newPassword} />
+      <SubmitButton>Confirm password change</SubmitButton>
     </form>
   );
 };
