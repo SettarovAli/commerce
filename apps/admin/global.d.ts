a89edf930a6cf6d0bf64f407c373bff8d9881a3e
SettarovAli <settarov.ali@gmail.com>
2024-11-03 23:16:44 +0200

    chore: update .env.exmaple and global.d.ts with new env variables for jwt auth

--- apps/admin/global.d.ts
+++ apps/admin/global.d.ts
@@ -1,5 +1,15 @@
 namespace NodeJS {
   interface ProcessEnv {
     SITE_NAME: string;
+
+    NEXT_PUBLIC_FIREBASE_API_KEY: string;
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+    NEXT_PUBLIC_FIREBASE_APP_ID: string;
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL: string;
+
+    JWT_SECRET_KEY: string;
   }
 }
