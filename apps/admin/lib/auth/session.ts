d1a3b26bb4aeed5f0ebc3f344d7ad7b5114646b4
SettarovAli <settarov.ali@gmail.com>
2024-11-04 23:47:46 +0200

    feat: add admin jwt auth session methods

---
+++ apps/admin/lib/auth/session.ts
@@ -0,0 +1,79 @@
+import 'server-only';
+
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+import { SignJWT, jwtVerify } from 'jose';
+import { SessionPayload } from 'lib/auth/types';
+import { Routes } from 'routes';
+
+const { JWT_SECRET_KEY } = process.env;
+
+const key = new TextEncoder().encode(JWT_SECRET_KEY);
+
+export const encrypt = async (payload: SessionPayload) => {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('1hr')
+    .sign(key);
+};
+
+export const decrypt = async (session: string = '') => {
+  try {
+    const { payload } = await jwtVerify(session, key, {
+      algorithms: ['HS256']
+    });
+    return payload;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const createSession = async (userId: string) => {
+  const expiresAt = new Date(Date.now() + 60 * 60 * 1000);
+  const session = await encrypt({ userId, expiresAt });
+
+  (await cookies()).set('session', session, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: 'lax',
+    path: '/'
+  });
+
+  redirect(Routes.Home);
+};
+
+export const verifySession = async () => {
+  const cookie = (await cookies()).get('session')?.value;
+  const session = await decrypt(cookie);
+
+  if (!session?.userId) {
+    redirect(Routes.SignIn);
+  }
+
+  return { isAuth: true, userId: String(session.userId) };
+};
+
+export const updateSession = async () => {
+  const session = (await cookies()).get('session')?.value;
+  const payload = await decrypt(session);
+
+  if (!session || !payload) {
+    return null;
+  }
+
+  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  (await cookies()).set('session', session, {
+    httpOnly: true,
+    secure: true,
+    expires: expires,
+    sameSite: 'lax',
+    path: '/'
+  });
+};
+
+export const deleteSession = async () => {
+  (await cookies()).delete('session');
+  redirect(Routes.SignIn);
+};
