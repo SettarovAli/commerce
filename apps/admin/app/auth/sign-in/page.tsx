e17c9803f2983527f7a7827df2031429530dee42
SettarovAli <settarov.ali@gmail.com>
2024-11-06 05:08:04 +0200

    feat: add sign in page

---
+++ apps/admin/app/auth/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import SignInForm from 'modules/auth/components/sign-in-form';
+
+const SignInPage = () => {
+  return (
+    <div className="fixed flex h-screen w-full items-center justify-center bg-gradient-to-br from-indigo-50 via-white to-cyan-100">
+      <SignInForm />
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ apps/admin/modules/auth/components/sign-in-form/sign-in-form.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useHookFormAction } from '@next-safe-action/adapter-react-hook-form/hooks';
+
+import { Form } from '@/components/ui/form';
+import { SubmitButton } from '@/components/common/form/submit-button';
+import { InputField } from '@/components/common/form/input-field';
+import { signInAction } from '@/lib/auth/actions';
+import { signInSchema } from '@/lib/auth/schemas';
+import { Routes } from 'routes';
+
+const SignInForm = () => {
+  const router = useRouter();
+
+  const { form, action, handleSubmitWithAction } = useHookFormAction(
+    signInAction,
+    zodResolver(signInSchema),
+    {
+      formProps: {
+        defaultValues: { email: '', password: '' }
+      },
+      actionProps: {
+        onSuccess: ({ data }) => {
+          toast.success(data?.message);
+          router.push(Routes.Home);
+        },
+        onError: ({ error }) => {
+          toast.error(error.serverError);
+        }
+      }
+    }
+  );
+
+  return (
+    <Form {...form}>
+      <form
+        className="mx-auto flex w-3/4 max-w-[400px] flex-col gap-4"
+        onSubmit={handleSubmitWithAction}
+        noValidate
+      >
+        <InputField type="email" label="Email" name="email" />
+        <InputField type="password" label="Password" name="password" />
+        <SubmitButton loading={action.isExecuting}>Sign In</SubmitButton>
+      </form>
+    </Form>
+  );
+};
+
+export default SignInForm;
