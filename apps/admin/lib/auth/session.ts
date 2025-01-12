907a578383deca182ecc10fb7a0a0b5e4d648e53
SettarovAli <settarov.ali@gmail.com>
2025-01-12 04:19:25 +0200

    fix: session utils for api routes

--- apps/admin/lib/auth/session.ts
+++ apps/admin/lib/auth/session.ts
@@ -30,25 +30,27 @@ const getSessionFromMiddleware = (req: NextRequest): Session => {
   return req.cookies.get(SESSION_COOKIE_NAME)?.value as Session;
 };
 
-const getSessionCookieOptions = (expiresAt?: Date): SessionCookieOptions => ({
+const getSessionCookieOptions = (expiresAt: Date): SessionCookieOptions => ({
   httpOnly: true,
   secure: true,
-  expires: expiresAt || getSessionExpirationTime(),
+  expires: expiresAt,
   sameSite: 'lax',
   path: '/'
 });
 
-const setSession = async (session: string, expiresAt?: Date): Promise<void> => {
+export const setSession = async (session: string): Promise<void> => {
+  const expiresAt = getSessionExpirationTime();
   const options = getSessionCookieOptions(expiresAt);
   (await cookies()).set(SESSION_COOKIE_NAME, session, options);
 };
 
-const setSessionFromMiddleware = (res: NextResponse, session: string, expiresAt?: Date): void => {
+const setSessionFromMiddleware = (res: NextResponse, session: string): void => {
+  const expiresAt = getSessionExpirationTime();
   const options = getSessionCookieOptions(expiresAt);
   res.cookies.set(SESSION_COOKIE_NAME, session, options);
 };
 
-export const encrypt = async (payload: SessionPayload): Promise<string> => {
+export const encryptSession = async (payload: SessionPayload): Promise<string> => {
   const expirationTime = Math.floor(Date.now() / 1000) + SESSION_EXPIRATION_TIME / 1000;
   return new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256' })
@@ -57,7 +59,7 @@ export const encrypt = async (payload: SessionPayload): Promise<string> => {
     .sign(key);
 };
 
-export const decrypt = async (session: string = ''): Promise<SessionDataPayload | null> => {
+const decryptSession = async (session: string = ''): Promise<SessionDataPayload | null> => {
   try {
     const { payload } = await jwtVerify(session, key, {
       algorithms: ['HS256']
@@ -70,29 +72,14 @@ export const decrypt = async (session: string = ''): Promise<SessionDataPayload 
 
 export const getSessionData = async (): Promise<SessionData> => {
   const session = await getSession();
-  const sessionPayload = await decrypt(session);
+  const sessionPayload = await decryptSession(session);
   return { session, sessionPayload };
 };
 
 export const getSessionDataFromMiddleware = async (req: NextRequest): Promise<SessionData> => {
   const session = getSessionFromMiddleware(req);
-  const sessionPayload = await decrypt(session);
+  const sessionPayload = await decryptSession(session);
   return { session, sessionPayload };
-};
-
-export const createSession = async (userId: string): Promise<void> => {
-  const expiresAt = getSessionExpirationTime();
-  const session = await encrypt({ userId, expiresAt });
-  await setSession(session, expiresAt);
-};
-
-export const createSessionFromMiddleware = async (
-  res: NextResponse,
-  userId: string
-): Promise<void> => {
-  const expiresAt = getSessionExpirationTime();
-  const session = await encrypt({ userId, expiresAt });
-  setSessionFromMiddleware(res, session, expiresAt);
 };
 
 export const verifySession = cache(async (): Promise<SessionDataPayload> => {
@@ -106,21 +93,14 @@ export const verifySession = cache(async (): Promise<SessionDataPayload> => {
   return sessionPayload;
 });
 
-export const updateSession = async (): Promise<void> => {
-  const { session, sessionPayload } = await getSessionData();
-  const userId = sessionPayload?.userId;
-  if (!session || !userId) return;
-  await createSession(userId);
-};
-
 export const updateSessionFromMiddleware = async (
   req: NextRequest,
   res: NextResponse
 ): Promise<void> => {
-  const { session, sessionPayload } = await getSessionDataFromMiddleware(req);
-  const userId = sessionPayload?.userId;
-  if (!session || !userId) return;
-  await createSessionFromMiddleware(res, sessionPayload.userId);
+  const { sessionPayload } = await getSessionDataFromMiddleware(req);
+  if (!sessionPayload) return;
+  const session = await encryptSession(sessionPayload);
+  setSessionFromMiddleware(res, session);
 };
 
 export const deleteSession = async (): Promise<never> => {
