100b05da3e25ddde788427c649cda6a1b7f140fc
SettarovAli <settarov.ali@gmail.com>
2025-01-15 22:48:32 +0200

    feat(store): update opengraph-images with shopify service

--- apps/store/app/[page]/opengraph-image.tsx
+++ apps/store/app/[page]/opengraph-image.tsx
@@ -1,10 +1,10 @@
-import OpengraphImage from 'components/opengraph-image';
-import { getPage } from 'lib/shopify';
+import OpengraphImage from '@/components/opengraph-image';
+import { shopifyService } from '@/lib/shopify/services/shopify-service';
 
 export const runtime = 'edge';
 
 export default async function Image({ params }: { params: { page: string } }) {
-  const page = await getPage(params.page);
+  const page = await shopifyService.getPage(params.page);
   const title = page.seo?.title || page.title;
 
   return await OpengraphImage({ title });
--- apps/store/app/search/[collection]/opengraph-image.tsx
+++ apps/store/app/search/[collection]/opengraph-image.tsx
@@ -1,10 +1,10 @@
-import OpengraphImage from 'components/opengraph-image';
-import { getCollection } from 'lib/shopify';
+import OpengraphImage from '@/components/opengraph-image';
+import { shopifyService } from '@/lib/shopify/services/shopify-service';
 
 export const runtime = 'edge';
 
 export default async function Image({ params }: { params: { collection: string } }) {
-  const collection = await getCollection(params.collection);
+  const collection = await shopifyService.getCollection(params.collection);
   const title = collection?.seo?.title || collection?.title;
 
   return await OpengraphImage({ title });
