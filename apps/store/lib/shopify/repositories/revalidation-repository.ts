e120c3745a2c1f4937330f71faedabc4083913ed
SettarovAli <settarov.ali@gmail.com>
2025-01-15 22:38:58 +0200

    feat: add revalidation repository for store app

---
+++ apps/store/lib/shopify/repositories/revalidation-repository.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { revalidateTag } from 'next/cache';
+import { headers } from 'next/headers';
+
+import { ShopifyRepository } from '@/lib/shopify/repositories/shopify-repository';
+import { TAGS } from '@/lib/constants';
+
+export class RevalidationRepository extends ShopifyRepository {
+  private revalidationSecret: string;
+
+  constructor() {
+    super();
+    this.revalidationSecret = process.env.SHOPIFY_REVALIDATION_SECRET || '';
+  }
+
+  // This is called from `app/api/revalidate.ts` so providers can control revalidation logic.
+  async revalidate(req: NextRequest): Promise<NextResponse> {
+    // We always need to respond with a 200 status code to Shopify,
+    // otherwise it will continue to retry the request.
+    const collectionWebhooks = ['collections/create', 'collections/delete', 'collections/update'];
+    const productWebhooks = ['products/create', 'products/delete', 'products/update'];
+    const topic = (await headers()).get('x-shopify-topic') || 'unknown';
+    const secret = req.nextUrl.searchParams.get('secret');
+    const isCollectionUpdate = collectionWebhooks.includes(topic);
+    const isProductUpdate = productWebhooks.includes(topic);
+
+    if (!secret || secret !== this.revalidationSecret) {
+      console.error('Invalid revalidation secret.');
+      return NextResponse.json({ status: 401 });
+    }
+
+    if (!isCollectionUpdate && !isProductUpdate) {
+      // We don't need to revalidate anything for any other topics.
+      return NextResponse.json({ status: 200 });
+    }
+
+    if (isCollectionUpdate) {
+      revalidateTag(TAGS.collections);
+    }
+
+    if (isProductUpdate) {
+      revalidateTag(TAGS.products);
+    }
+
+    return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
+  }
+}
