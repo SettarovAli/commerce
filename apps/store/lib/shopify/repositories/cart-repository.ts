8a9119a8996573b367455f228cdab872ba8a529a
SettarovAli <settarov.ali@gmail.com>
2025-01-15 22:40:17 +0200

    feat: add cart repository for store app

---
+++ apps/store/lib/shopify/repositories/cart-repository.ts
@@ -0,0 +1,98 @@
+import { ShopifyRepository } from '@/lib/shopify/repositories/shopify-repository';
+import { getCartQuery } from '@/lib/shopify/queries/cart';
+import {
+  addToCartMutation,
+  createCartMutation,
+  editCartItemsMutation,
+  removeFromCartMutation
+} from '@/lib/shopify/mutations/cart';
+import { TAGS } from '@/lib/constants';
+import {
+  Cart,
+  ShopifyAddToCartOperation,
+  ShopifyCart,
+  ShopifyCartOperation,
+  ShopifyCreateCartOperation,
+  ShopifyRemoveFromCartOperation,
+  ShopifyUpdateCartOperation
+} from '@/lib/shopify/types';
+
+export class CartRepository extends ShopifyRepository {
+  async createCart(): Promise<Cart> {
+    const res = await this.fetch<ShopifyCreateCartOperation>({
+      query: createCartMutation,
+      cache: 'no-store'
+    });
+
+    return this.reshapeCart(res.body.data.cartCreate.cart);
+  }
+
+  async addToCart(
+    cartId: string,
+    lines: { merchandiseId: string; quantity: number }[]
+  ): Promise<Cart> {
+    const res = await this.fetch<ShopifyAddToCartOperation>({
+      query: addToCartMutation,
+      variables: { cartId, lines },
+      cache: 'no-store'
+    });
+
+    return this.reshapeCart(res.body.data.cartLinesAdd.cart);
+  }
+
+  async removeFromCart(cartId: string, lineIds: string[]): Promise<Cart> {
+    const res = await this.fetch<ShopifyRemoveFromCartOperation>({
+      query: removeFromCartMutation,
+      variables: { cartId, lineIds },
+      cache: 'no-store'
+    });
+
+    return this.reshapeCart(res.body.data.cartLinesRemove.cart);
+  }
+
+  async updateCart(
+    cartId: string,
+    lines: { id: string; merchandiseId: string; quantity: number }[]
+  ): Promise<Cart> {
+    const res = await this.fetch<ShopifyUpdateCartOperation>({
+      query: editCartItemsMutation,
+      variables: { cartId, lines },
+      cache: 'no-store'
+    });
+
+    return this.reshapeCart(res.body.data.cartLinesUpdate.cart);
+  }
+
+  async getCart(cartId: string | undefined): Promise<Cart | undefined> {
+    if (!cartId) {
+      return undefined;
+    }
+
+    const res = await this.fetch<ShopifyCartOperation>({
+      query: getCartQuery,
+      variables: { cartId },
+      tags: [TAGS.cart]
+    });
+
+    // Old carts becomes `null` when you checkout.
+    if (!res.body.data.cart) {
+      return undefined;
+    }
+
+    return this.reshapeCart(res.body.data.cart);
+  }
+
+  private reshapeCart(cart: ShopifyCart): Cart {
+    if (!cart.cost?.totalTaxAmount) {
+      cart.cost.totalTaxAmount = {
+        amount: '0.0',
+        currencyCode: cart.cost.totalAmount.currencyCode
+      };
+    }
+
+    return {
+      ...cart,
+      lines: this.removeEdgesAndNodes(cart.lines)
+    };
+  }
+}
