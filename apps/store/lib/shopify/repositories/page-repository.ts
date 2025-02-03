d295ef6d4d557c3302da6cfe1ff401322e01289e
SettarovAli <settarov.ali@gmail.com>
2025-02-03 21:37:09 +0200

    feat(store): update page repository with new fetcher

--- apps/store/lib/shopify/repositories/page-repository.ts
+++ apps/store/lib/shopify/repositories/page-repository.ts
@@ -1,24 +1,26 @@
+import 'server-only';
+
 import { ShopifyRepository } from '@/lib/shopify/repositories/shopify-repository';
 import { getPageQuery, getPagesQuery } from '@/lib/shopify/queries/page';
 import { Page, ShopifyPageOperation, ShopifyPagesOperation } from '@/lib/shopify/types';
 
 export class PageRepository extends ShopifyRepository {
   async getPage(handle: string): Promise<Page> {
-    const res = await this.fetch<ShopifyPageOperation>({
+    const res = await this.get<ShopifyPageOperation>({
       query: getPageQuery,
       cache: 'no-store',
       variables: { handle }
     });
 
-    return res.body.data.pageByHandle;
+    return res.data.pageByHandle;
   }
 
   async getPages(): Promise<Page[]> {
-    const res = await this.fetch<ShopifyPagesOperation>({
+    const res = await this.get<ShopifyPagesOperation>({
       query: getPagesQuery,
       cache: 'no-store'
     });
 
-    return this.removeEdgesAndNodes(res.body.data.pages);
+    return this.removeEdgesAndNodes(res.data.pages);
   }
 }
